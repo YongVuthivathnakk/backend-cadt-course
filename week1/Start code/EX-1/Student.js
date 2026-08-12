a216ba0fbb2b8b621964bf2897c8f39de3ae5122
YongVuthivathnakk <vuthivathnakkyong@gmail.com>
2026-08-12 16:40:39 +0700

    fix(add-week1-missing-files): add missing week1 files

---
+++ week1/Start code/EX-1/Student.js
@@ -0,0 +1,14 @@
+import fs from 'fs/promises';
+
+const filePath = './hello.txt';
+
+try {
+  // Write to a file (asynchronously)
+  await fs.writeFile(filePath, "Hello, Node.js beginner!");
+
+  // Read the file (asynchronously)
+  const content = await fs.readFile(filePath, "utf8");
+  console.log("File content:", content);
+} catch (err) {
+  console.error("Error:", err);
+}
